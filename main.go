75d274991dc68e09e44334bed5adebaaaabaf6e2
EfrenAl <eal@tier.app>
2021-12-12 19:28:38 +0100

    Fix5

--- main.go
+++ main.go
@@ -12,8 +12,57 @@ import (
 
 func main() {
     
-	r := router.Router()
-    fmt.Println("Starting server on the port 8080...")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	//r := router.Router()
+    //fmt.Println("Starting server on the port 8080...")
+    //log.Fatal(http.ListenAndServe(":8080", r))
+
+	router = gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
 
 }
+
+
+
+/*
+
+func main() {
+    port := os.Getenv("PORT")
+
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
+
+    tStr := os.Getenv("REPEAT")
+    repeat, err := strconv.Atoi(tStr)
+    if err != nil {
+        log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
+        repeat = 5
+    }
+
+    db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+    if err != nil {
+        log.Fatalf("Error opening database: %q", err)
+    }
+
+    router := gin.New()
+    router.Use(gin.Logger())
+    router.LoadHTMLGlob("templates/*.tmpl.html")
+    router.Static("/static", "static")
+
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl.html", nil)
+    })
+
+    router.GET("/mark", func(c *gin.Context) {
+        //c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
+    })
+
+    router.GET("/repeat", repeatHandler(repeat))
+
+    router.GET("/db", dbFunc(db))
+
+    router.Run(":" + port)
+}
+
+*/
