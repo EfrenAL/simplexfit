f6f946b13a0dac43fd55347f7c24fb56d17ec544
EfrenAl <eal@tier.app>
2021-12-12 21:42:41 +0100

    Add tables and controllers

--- router/routes.go
+++ router/routes.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/heroku/go-getting-started/controllers"
 )
@@ -10,14 +8,8 @@ func Routes(router *gin.Engine) {
 	router.GET("/exercise", controllers.GetAllExercise)
 	router.POST("/exercise", controllers.CreateExercise)
 	router.GET("/exercise/:exerciseId", controllers.GetSingleExercise)
-	router.PUT("/exercise/:exerciseId", controllers.EditExercise)
 	router.DELETE("/exercise/:exerciseId", controllers.DeleteExercise)
 }
-
-func welcome(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status":  200,"message": "Welcome To API",})
-}
-
 
 /*
 package router
